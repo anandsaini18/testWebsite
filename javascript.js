2947f91b47e5dda08a71c680b408af23f55930d0
anandsaini18 <53001346+anandsaini18@users.noreply.github.com>
2019-07-17 16:11:11 +0530

    added index.html

---
+++ javascript.js
@@ -0,0 +1,11 @@
+function call(){
+    this.correct();
+}
+function correct(){
+    var name,x;
+    var dob = new Date();
+    name=document.getElementById("name").value;
+    
+    dob=document.getElementById("dob");
+    //alert("Signed up as "+name);
+}
